e6a9e664a8b5e1983bc1cd6ee862cd75a76c6314
Baileyton <baileyton@naver.com>
2023-12-07 16:52:20 +0900

    회원 도메인 실행과 테스트

---
+++ src/main/java/spring/core/MemberApp.java
@@ -0,0 +1,19 @@
+package spring.core;
+
+import spring.core.Member.Grade;
+import spring.core.Member.Member;
+import spring.core.Member.MemberService;
+import spring.core.Member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
